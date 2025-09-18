395c7e27ce32fe26f8a36d537f3486fa2b1357be
Camila Germano <c.germano@iesb.edu.br>
2025-09-18 10:49:20 -0300

    codigos da aula 7

--- aulas/aula07/app.js
+++ aulas/aula07/app.js
@@ -13,4 +13,25 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 
+app.get('/tarefas', (req, res) => {
+    res.send("Ok")
+})
+
+
+app.post("/tarefas", (req, res) => {
+    const novaTarefa = {
+        id: tarefas.length + 1,
+        nome: req.body.nome,
+        concluida: false,
+    }
+    tarefas.push(novaTarefa)
+    res.status(201).json(novaTarefa)
+})
+
+app.get('/tarefas/:id', (req, res) => {
+    res.json({})
+})
+
+app.post('/tarefas/:id', (req, res))
+
 module.exports = app;
